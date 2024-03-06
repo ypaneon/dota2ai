23c2b1dbeac85c685759e2d1b859f3669858529b
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 03:23:47 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/bloodseeker.lua
+++ hero/items/bloodseeker.lua
@@ -7,24 +7,19 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_flask",
-	"item_quelling_blade",
-	"item_branches",
-	"item_branches",
-	"item_circlet",
 	"item_wraith_band",
 	"item_magic_wand",
-	"item_phase_boots", --相位
-	-- "item_blade_mail", --刃甲
+	"item_phase_boots", 
 	"item_maelstrom",
 	"item_sange_and_yasha",
 	"item_aghanims_shard", 
-	"item_black_king_bar", --bkb
-	"item_basher", --晕锤7.14
-	"item_mjollnir", --大电锤
-	"item_abyssal_blade", --大晕锤
-	"item_butterfly", --蝴蝶
-	"item_ultimate_scepter_2"
+	"item_black_king_bar",
+	"item_basher",
+	"item_mjollnir",
+	"item_abyssal_blade",
+	"item_butterfly",
+	"item_ultimate_scepter_2",
+  	"item_travel_boots_2",
 }
 
 return X
