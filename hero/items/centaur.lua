47d766adfd8d613b1d3ae5cbdaab59612bb6b980
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 03:33:51 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/centaur.lua
+++ hero/items/centaur.lua
@@ -7,22 +7,20 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_flask",
-	"item_enchanted_mango",
 	"item_magic_wand",
-	-- "item_bracer",
-	"item_vanguard", 
+	"item_bracer", 
 	"item_phase_boots",
-	-- "item_bracer",
 	"item_blink", --跳刀
-	"item_crimson_guard", --赤红甲
+	"item_veil_of_discord",
+	"item_shivas_guard",
 	"item_aghanims_shard",
-	"item_pipe", --笛子
 	"item_heart", --龙心7.20
+	"item_lotus_orb",
 	"item_ultimate_scepter", --蓝杖
 	"item_ultimate_scepter_2",
 	"item_overwhelming_blink",
 	"item_assault",
+	"item_travel_boots_2",
 }
 
 return X
