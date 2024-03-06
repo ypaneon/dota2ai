cd573324f0c679c5d40fb5b50a302d4329e39a2f
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 03:27:17 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/bristleback.lua
+++ hero/items/bristleback.lua
@@ -7,20 +7,19 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_enchanted_mango",
-	"item_enchanted_mango",
-	"item_quelling_blade",
-	-- "item_falcon_blade", 
-	"item_vanguard", --先锋
-	"item_phase_boots", --相位
+  	"item_magic_wand",
+  	"item_bracer",
+  	"item_phase_boots",
+  	"item_vanguard",
+	"item_eternal_shroud",
+  	"item_kaya_and_sange",
+	"item_heart",
+  	"item_bloodstone",
+  	"item_crimson_guard",
+  	"item_aghanims_shard",
 	"item_ultimate_scepter",
-	-- "item_magic_wand",
-	-- "item_crimson_guard",
-	"item_heart", --龙心7.20
-	"item_black_king_bar", --BKB
-	"item_shivas_guard",
 	"item_ultimate_scepter_2",
-	"item_lotus_orb",
+  	"item_travel_boots_2",
 }
 
 return X
