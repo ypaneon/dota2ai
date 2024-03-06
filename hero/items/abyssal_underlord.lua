c7a893874313810172adc618802f5c65a73637fe
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 03:00:17 +0630

    abyssal_underlord.lua

--- hero/items/abyssal_underlord.lua
+++ hero/items/abyssal_underlord.lua
@@ -7,19 +7,19 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
-	"item_enchanted_mango",
-	"item_boots",
-	"item_magic_wand", --大魔棒7.14
 	"item_bracer",
+	"item_magic_wand", 
 	"item_arcane_boots",
-	"item_mekansm", --梅肯
-	"item_hood_of_defiance", --挑战
-	"item_crimson_guard", --赤红甲
-	"item_pipe", --笛子
-	"item_guardian_greaves", --卫士胫甲
-	"item_lotus_orb", --清莲宝珠
-	"item_shivas_guard" --希瓦
+  	"item_vanguard",
+	"item_veil_of_discord",
+	"item_shivas_guard",
+  	"item_crimson_guard",
+  	"item_pipe",
+  	"item_guardian_greaves",
+  	"item_heart",
+  	"item_aghanims_shard",
+  	"item_ultimate_scepter",
+  	"item_ultimate_scepter_2",
 }
 
 return X
