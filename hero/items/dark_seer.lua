88790a34e032f4c5c713ed73c716f7b0fb175883
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 03:59:49 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/dark_seer.lua
+++ hero/items/dark_seer.lua
@@ -7,21 +7,19 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_flask",
-	"item_enchanted_mango",
-	"item_soul_ring", --魂戒
-	"item_boots",
 	"item_magic_wand", --大魔棒7.14
+	"item_veil_of_discord",
 	"item_arcane_boots",
 	"item_mekansm", --梅肯
+	"item_shivas_guard", --希瓦
 	"item_guardian_greaves", --卫士胫甲
 	"item_ultimate_scepter",
 	"item_blink", --跳刀
+	"item_heart" --龙心7.20
 	"item_lotus_orb", --清莲宝珠
-	"item_shivas_guard", --希瓦
 	"item_arcane_blink",
 	"item_ultimate_scepter_2",
-	"item_heart" --龙心7.20
+	"item_travel_boots_2",
 }
 
 return X
