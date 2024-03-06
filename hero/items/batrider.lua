7216d9f84ba35082f51137727adfe4df497339da
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 03:21:41 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/batrider.lua
+++ hero/items/batrider.lua
@@ -7,7 +7,6 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_wind_lace",
 	"item_bracer",
 	"item_magic_wand",
 	"item_tranquil_boots",
@@ -15,11 +14,10 @@ X.ItemsToBuy =
 	"item_force_staff", --推推7.14
 	"item_ultimate_scepter", --蓝杖
 	"item_black_king_bar", --bkb
-	"item_cyclone", --风杖
+  	"item_octarine_core",
 	"item_ultimate_scepter_2",
-	"item_lotus_orb",
 	"item_arcane_blink",
-	"item_wind_waker",
+  	"item_travel_boots_2",
 }
 
 return X
