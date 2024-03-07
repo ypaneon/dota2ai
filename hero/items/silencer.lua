307d5012a77add0f4a9642ffef96ce4a6d46ddab
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 16:53:01 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/silencer.lua
+++ hero/items/silencer.lua
@@ -7,22 +7,21 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	-- "item_tango",
 	"item_null_talisman",
 	"item_null_talisman", 
-	"item_magic_stick",
+	"item_magic_wand",
 	"item_power_treads", --假腿7.21
 	"item_witch_blade",
 	"item_dragon_lance",
+	"item_yasha_and_kaya",
 	"item_hurricane_pike", --大推推7.20
 	"item_aghanims_shard",
-	"item_orchid",
+	"item_devastator",
 	"item_sheepstick", --羊刀
-	-- "item_gungir",
 	"item_ultimate_scepter", --蓝杖
 	"item_ultimate_scepter_2",
 	"item_moon_shard",
-	"item_bloodthorn",
+	"item_travel_boots_2",
 }
 
 return X
