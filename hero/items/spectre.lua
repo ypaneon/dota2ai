ceda0a785875280bff43cbfbb8baac8f3fc09973
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 17:19:18 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/spectre.lua
+++ hero/items/spectre.lua
@@ -7,22 +7,19 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
-	"item_quelling_blade", --补刀斧
+	"item_magic_wand",
 	"item_wraith_band", --系带
-	"item_flask",
 	"item_phase_boots",
-	"item_bfury",
-	-- "item_orb_of_corrosion",
 	"item_diffusal_blade", --散失刀
 	"item_manta", --分身
 	"item_heart", --龙心7.20
-	"item_black_king_bar",
-	"item_ultimate_scepter",
+	"item_diesperser",
 	"item_butterfly",
+	"item_skadi",
+	"item_ultimate_scepter",
 	"item_aghanims_shard",
 	"item_ultimate_scepter_2",
-	"item_skadi",
+	"item_travel_boots_2",
 }
 
 return X
