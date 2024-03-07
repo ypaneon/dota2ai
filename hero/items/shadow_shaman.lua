2e577eee4fa1714038983009560510269bdbf9fa
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 16:45:57 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/shadow_shaman.lua
+++ hero/items/shadow_shaman.lua
@@ -7,21 +7,17 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
-	"item_clarity",
-	"item_magic_stick",
+	"item_magic_wand",
 	"item_arcane_boots",
+	"item_phylactery",
 	"item_blink",
-	"item_aether_lens",
-	"item_ghost",
-	"item_force_staff",
+	"item_glimmer_cape",
 	"item_aghanims_shard",
+	"item_angels_demise",
 	"item_ultimate_scepter", --蓝杖
-	"item_lotus_orb", --清莲宝珠
-	"item_ultimate_scepter_2",
 	"item_sheepstick", --羊刀
-	"item_octarine_core",
 	"item_arcane_blink",
+	"item_travel_boots_2",
 }
 
 return X
