09ae17b23253f688cca623709cc5d2ea8afd8649
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 17:32:20 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/templar_assassin.lua
+++ hero/items/templar_assassin.lua
@@ -8,16 +8,17 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_wraith_band", --系带
-	"item_flask",
-	"item_power_treads", --假腿7.21
 	"item_magic_wand",
+	"item_wraith_band", --系带
+	"item_power_treads", --假腿7.21
 	"item_desolator", --黯灭
 	"item_black_king_bar", --bkb
 	"item_blink",
+	"item_aghanims_shard",
+	"item_greater_crit",
 	"item_butterfly", --蝴蝶
-	"item_bloodthorn",
 	"item_swift_blink",
+	"item_travel_boots_2",
 }
 
 return X
