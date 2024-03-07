bd06545e603f335b93eb60e4422fae5d15269fcb
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 10:29:18 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/doom_bringer.lua
+++ hero/items/doom_bringer.lua
@@ -7,19 +7,18 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_flask",
-	-- "item_bracer",
-	"item_boots",
+	"item_bracer",
 	"item_magic_wand",
 	"item_phase_boots",
-	"item_hand_of_midas",
-	"item_ancient_janggo", --战鼓7.20
-	"item_invis_sword", --隐刀
+	"item_veil_of_discord",
 	"item_shivas_guard", --希瓦
+	"item_radiance",
+	"item_heart",
+	"item_octarine_core",
 	"item_ultimate_scepter",
 	"item_ultimate_scepter_2",
 	"item_aghanims_shard",
-	"item_silver_edge", --大隐刀
+	
 }
 
 return X
