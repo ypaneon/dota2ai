7c177ccc59c9cb82f9fda7aabfe911b5caca617c
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 17:48:47 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/troll_warlord.lua
+++ hero/items/troll_warlord.lua
@@ -8,17 +8,20 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
-	"item_quelling_blade",
-	"item_wraith_band",
 	"item_wraith_band",
 	"item_magic_wand",
 	"item_phase_boots", --相位7.21
-	"item_diffusal_blade", --散失刀
+	"item_javelin",
+	"item_maelstrom",
 	"item_sange_and_yasha", --双刀
+	"item_mjollnir",
 	"item_black_king_bar", --bkb
 	"item_butterfly",
-	"item_monkey_king_bar",
+	"item_satanic",
+	"item_aghanims_shard",
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_travel_boots_2",
 }
 
 return X
