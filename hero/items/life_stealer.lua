e8f890f9074cb9d6afb5585b29210b7a95536794
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 13:40:44 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/life_stealer.lua
+++ hero/items/life_stealer.lua
@@ -7,19 +7,20 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
-	"item_magic_stick",
-	"item_quelling_blade", --补刀斧
-	"item_orb_of_venom",
-	"item_phase_boots", --相位7.21
+	"item_magic_wand",
+	"item_bracer",
 	"item_orb_of_corrosion",
-	"item_armlet",
-	"item_aghanims_shard",
+	"item_phase_boots", --相位7.21
 	"item_desolator",
 	"item_basher",
-	"item_abyssal_blade",
 	"item_sange_and_yasha",
+	"item_abyssal_blade",
 	"item_assault"
+	"item_heart",
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
