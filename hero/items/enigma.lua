49870b7be8c52a46de17df73064b562c823c6f80
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 11:17:00 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/enigma.lua
+++ hero/items/enigma.lua
@@ -7,21 +7,19 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_clarity",
 	"item_magic_wand", --大魔棒7.14
 	"item_arcane_boots", --秘法鞋
 	"item_blink", --跳刀
 	"item_black_king_bar", --bkb
 	"item_guardian_greaves", --卫士胫甲
 	"item_aghanims_shard",
-	"item_vitality_booster",
-	"item_energy_booster",
-	"item_aeon_disk", -- 永恒之盘
 	"item_ultimate_scepter", --蓝杖
 	"item_octarine_core", --玲珑心
 	"item_ultimate_scepter_2",
 	"item_refresher",
 	"item_arcane_blink",
+	"item_travel_boots_2",
+	
 }
 
 return X
