02a55958c17501ca844cb8f26655313fe968e1f8
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 14:59:09 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/mirana.lua
+++ hero/items/mirana.lua
@@ -8,7 +8,6 @@ X.ItemsToBuy =
 {
 	"item_tango",
 	"item_wraith_band", --系带
-	"item_flask",
 	"item_magic_wand",
 	"item_phase_boots", --相位7.21
 	"item_maelstrom",
@@ -17,6 +16,10 @@ X.ItemsToBuy =
 	"item_butterfly",
 	"item_mjollnir", --大雷锤
 	"item_greater_crit",
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
