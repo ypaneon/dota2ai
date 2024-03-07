4e4dd0efc16b3f812b213ba0c8d8ef37d2cf8816
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 16:31:55 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/razor.lua
+++ hero/items/razor.lua
@@ -6,30 +6,19 @@ local X = {}
 
 X.ItemsToBuy =
 {
-	-- "item_tango",
-	-- "item_wraith_band", --系带
-	-- "item_flask",
-	-- "item_magic_wand", --大魔棒7.14
-	-- "item_phase_boots", --相位7.21
-	-- "item_sange_and_yasha", --双刀
-	-- "item_black_king_bar", --bkb
-	-- "item_ultimate_scepter", --蓝杖
-	-- "item_heart",
-	-- "item_ultimate_scepter_2",
-	-- "item_shivas_guard" --希瓦
 	"item_tango",
-	"item_wraith_band",
-	"item_flask",
-	"item_magic_wand",
-	"item_arcane_boots",
-	"item_eternal_shroud",
+	"item_wraith_band", --系带
+	"item_magic_wand", --大魔棒7.14
+	"item_phase_boots", --相位7.21
+	"item_sange_and_yasha", --双刀
+	"item_black_king_bar", --bkb
 	"item_aghanims_shard",
-	"item_bloodstone",
-	"item_kaya_and_sange",
-	"item_heart",
-	"item_ultimate_scepter",
-	"item_ultimate_scepter_2"
-	
+	"item_satanic",
+	"item_butterfly",
+	"item_ultimate_scepter", --蓝杖
+	"item_ultimate_scepter_2",
+	"item_assault",
+	"item_travel_boots_2",
 	
 }
 
