e8c188d8b45319063013d2c1e41e0418330746f5
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 10:14:55 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/dazzle.lua
+++ hero/items/dazzle.lua
@@ -7,20 +7,14 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_branches",
-	"item_branches",
-	"item_branches",
-	-- "item_wind_lace",
-	"item_boots",
 	"item_magic_wand", --大魔棒7.14
 	"item_arcane_boots",
-	"item_spirit_vessel", --大骨灰
-	"item_mekansm", --梅肯
+	"item_phylactery",
+	"item_kaya_and_sange",
 	"item_aghanims_shard",
+	"item_angels_demise",
 	"item_ultimate_scepter", 
-	-- "item_ghost",
 	"item_guardian_greaves", --卫士胫甲
-	-- "item_solar_crest", --大勋章7.20
 	"item_sheepstick", --羊刀
 	"item_ultimate_scepter_2"
 }
