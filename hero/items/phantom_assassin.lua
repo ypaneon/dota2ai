487caf99eb308327468a7c7d614c38ec5babe082
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 16:06:31 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/phantom_assassin.lua
+++ hero/items/phantom_assassin.lua
@@ -12,14 +12,18 @@ X.ItemsToBuy =
 	"item_phase_boots", --相位7.21
 	"item_magic_wand", --大魔棒7.14
 	"item_orb_of_corrosion",
+	"item_phylactery",
 	"item_bfury",
 	"item_desolator",
-	"item_aghanims_shard",
 	"item_black_king_bar", --bkb
+	"item_angels_demise",
 	"item_basher",
 	"item_abyssal_blade", --大晕锤
 	"item_satanic", --撒旦7.07
-	"item_assault"
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
