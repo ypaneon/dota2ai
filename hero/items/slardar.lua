a74f63ede1740380576d8e3d1dda42659859a5c8
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 17:10:07 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/slardar.lua
+++ hero/items/slardar.lua
@@ -8,19 +8,18 @@ X.ItemsToBuy =
 {
 	"item_tango",
 	"item_bracer",
-	-- "item_flask",
-	-- "item_bracer",
-	-- "item_magic_wand",
-	"item_soul_ring",
+	"item_magic_wand",
 	"item_power_treads",
-	"item_blink", --跳刀
-	"item_aghanims_shard",
 	"item_echo_sabre", --连击刀
+	"item_blink", --跳刀
+	"item_harpoon",
 	"item_black_king_bar", --bkb
+	"item_aghanims_shard",
 	"item_assault", --强袭
-	"item_moon_shard", 
-	"item_octarine_core",
+	"item_ultimate_scepter_2",
+	"item_heart",
 	"item_overwhelming_blink",
+	"item_travel_boots_2",
 }
 
 return X
