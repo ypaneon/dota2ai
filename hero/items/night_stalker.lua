64ca19fe064d44a5d37db0828274f0f5ad3630ea
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 15:31:04 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/night_stalker.lua
+++ hero/items/night_stalker.lua
@@ -8,16 +8,19 @@ X.ItemsToBuy =
 {
 	"item_tango",
 	"item_bracer",
-	"item_flask",
-	"item_phase_boots", --相位7.21
 	"item_magic_wand",
-	"item_blink",
+	"item_phase_boots", --相位7.21
+	"item_echo_sabre",
+	"item_sange_and_yasha",
 	"item_black_king_bar", --bkb
-	"item_ultimate_scepter",
-	"item_assault", --强袭
-	"item_ultimate_scepter_2",
+	"item_basher",
+	"item_harpoon",
+	"item_abyssal_blade",
 	"item_heart",
-	"item_overwhelming_blink",
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
