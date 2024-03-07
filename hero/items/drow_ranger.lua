96fcddec1993b2bd78a37974334d572daf42392e
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 10:42:47 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/drow_ranger.lua
+++ hero/items/drow_ranger.lua
@@ -8,21 +8,21 @@ X.ItemsToBuy =
 {
 	"item_tango",
 	"item_wraith_band", --系带
-	"item_flask",
-	"item_power_treads",
 	"item_wraith_band", --系带
+	"item_power_treads",
 	"item_dragon_lance", --魔龙枪
+	"item_lesser_crit",
 	-- "item_aghanims_shard",
 	"item_ultimate_scepter", --蓝杖
 	-- "item_yasha", --夜叉
 	-- "item_mjollnir",
-	"item_silver_edge", 
 	"item_manta",
+	"item_greater_crit",
 	"item_hurricane_pike", --大推推7.20
 	"item_black_king_bar",
-	"item_monkey_king_bar",
-	"item_ultimate_scepter_2",
 	"item_butterfly",
+	"item_ultimate_scepter_2",
+	"item_travel_boots_2",
 }
 
 return X
