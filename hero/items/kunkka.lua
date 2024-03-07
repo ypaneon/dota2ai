6eb385239d1a369a86b64c306f07dfdb240a2e41
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 12:33:20 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/kunkka.lua
+++ hero/items/kunkka.lua
@@ -6,16 +6,19 @@ local X = {}
 
 X.ItemsToBuy = {
 	"item_tango",
-	"item_quelling_blade", --补刀斧
 	"item_bracer",
-	"item_phase_boots", --相位7.21
 	"item_magic_wand", --大魔棒7.14
-	"item_blade_mail",
+	"item_phase_boots", --相位7.21
+	"item_lesser_crit",
 	"item_black_king_bar", --bkb
-	"item_ultimate_scepter",
+	"item_bfury",
 	"item_greater_crit", --大炮
-	"item_ultimate_scepter_2",
 	"item_assault",
+	"item_heart",
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
