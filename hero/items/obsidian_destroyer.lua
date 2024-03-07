ef4ee97886c9d8ce12931e476f966e2fad0bf909
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 15:42:13 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/obsidian_destroyer.lua
+++ hero/items/obsidian_destroyer.lua
@@ -9,20 +9,19 @@ X.ItemsToBuy =
 {
 	"item_tango",
 	"item_null_talisman", --无用挂件
-	"item_null_talisman", --无用挂件
+	"item_magic_wand",
 	"item_power_treads", --假腿7.21
-	"item_dragon_lance", --魔龙枪
 	"item_witch_blade",
-	"item_orchid",
+	"item_dragon_lance", --魔龙枪
 	"item_black_king_bar", --bkb
-	"item_sheepstick", --羊刀
-	"item_aghanims_shard",
+	"item_devastator",
 	"item_hurricane_pike", --大推推7.20
-	"item_sphere", --林肯
-	"item_ultimate_scepter",
-	"item_ultimate_scepter_2",
 	"item_moon_shard",
-	"item_bloodthorn",
+	"item_ultimate_scepter",
+	"item_aghanims_shard",
+	"item_ultimate_scepter_2",
+	"item_sheepstick", --羊刀
+	"item_travel_boots_2",
 }
 
 return X
