118f0b8daf47494d31f76e3dc997aa5f1bd69161
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 15:48:33 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/ogre_magi.lua
+++ hero/items/ogre_magi.lua
@@ -7,18 +7,20 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_clarity",
-	"item_flask",
-	"item_gauntlets",
-	"item_boots",
+	"item_magic_wand",
+	"item_bracer",
 	"item_arcane_boots",
 	"item_hand_of_midas",
-	"item_aether_lens", --以太之镜7.06
-	"item_ultimate_scepter", --蓝杖
-	"item_aghanims_shard",
-	"item_force_staff", --推推7.14
+	"item_phylactery",
+	"item_kaya_and_sange", --以太之镜7.06
+	"item_angels_demise",
+	"item_heart",
+	"item_sheepstick",
+	"item_ultimate_scepter",
 	"item_ultimate_scepter_2",
-	"item_octarine_core",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
+	
 }
 
 return X
