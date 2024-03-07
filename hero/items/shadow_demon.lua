af562a55d7c51810e5bc59b9997ff97dc00ed628
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 16:42:36 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/shadow_demon.lua
+++ hero/items/shadow_demon.lua
@@ -8,16 +8,17 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
-	"item_clarity",
-	"item_magic_stick",
-	"item_tranquil_boots",
+	"item_null_talisman",
+	"item_magic_wand",
+	"item_arcane_boots",
 	"item_glimmer_cape", --微光
 	"item_force_staff",
 	"item_ultimate_scepter", --蓝杖
 	"item_lotus_orb", --清莲宝珠
-	"item_ultimate_scepter_2",
 	"item_sheepstick" --羊刀
+	"item_aghanims_shard",
+	"item_ultimate_scepter_2",
+	"item_travel_boots_2",
 }
 
 return X
