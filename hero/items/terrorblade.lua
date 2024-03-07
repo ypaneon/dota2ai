93639314a01133b3401bca983a2351b57c5141d5
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 17:37:03 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/terrorblade.lua
+++ hero/items/terrorblade.lua
@@ -8,16 +8,20 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
 	"item_wraith_band",
+	"item_magic_wand", --大魔棒7.14
 	"item_wraith_band",
 	"item_power_treads",
-	"item_magic_wand", --大魔棒7.14
-	"item_manta",
+	"item_lesser_crit",
 	"item_skadi", --冰眼
+	"item_greater_crit",
 	"item_black_king_bar", --bkb
 	"item_butterfly", --蝴蝶
 	"item_satanic" --撒旦7.07
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
