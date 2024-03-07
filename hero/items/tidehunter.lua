aa61a1b3359344bc86bf1084a33f0d8184d9e1a2
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 17:40:03 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/tidehunter.lua
+++ hero/items/tidehunter.lua
@@ -7,21 +7,18 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_quelling_blade",
-	"item_flask",
-	"item_enchanted_mango",
-	"item_bracer",
-	"item_soul_ring",
-	"item_phase_boots",
 	"item_magic_wand",
-	"item_blink", --跳刀
+	"item_bracer",
+	"item_phase_boots",
+	"item_mage_slayer",
 	"item_vladmir",
-	"item_medallion_of_courage",
-	"item_solar_crest",
-	"item_aghanims_shard",
-	"item_overwhelming_blink",
-	"item_helm_of_the_overlord", --level 2 helm dominator 7.30
-	"item_shivas_guard",
+	"item_ultimate_scepter",
+	"item_greater_crit",
+	"item_heart",
+	"item_ultimate_scepter_2",
+	"item_assault",
+	"item_travel_boots_2",
+	
 }
 
 return X
