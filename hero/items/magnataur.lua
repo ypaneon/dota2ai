5fcdd3cc7d282625362a16223dc9c5cc68645a73
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 14:46:30 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/magnataur.lua
+++ hero/items/magnataur.lua
@@ -8,17 +8,19 @@ X.ItemsToBuy =
 {
 	"item_tango",
 	"item_magic_wand", --大魔棒7.14
-	-- "item_bracer",
-	"item_phase_boots",
-	"item_blink",
-	"item_falcon_blade",
+	"item_wraith_band",
+	"item_power_treads",
 	"item_echo_sabre", --连击刀
+	"item_blink",
 	"item_black_king_bar", --bkb
 	"item_greater_crit", --大炮
-	"item_ultimate_scepter", --A
-	"item_ultimate_scepter_2",
-	"item_assault",
+	"item_harpoon",
+	"item_heart",
+	"item_aghanims_shard",
 	"item_overwhelming_blink",
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_travel_boots_2",
 }
 
 return X
