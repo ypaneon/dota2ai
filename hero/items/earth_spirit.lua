f0bc5fa3c9cdbbf6f4e646a22ea017dc78b64513
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 10:49:36 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/earth_spirit.lua
+++ hero/items/earth_spirit.lua
@@ -7,20 +7,18 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_clarity",
-	"item_orb_of_venom", --毒球
-	"item_boots",
 	"item_bracer",
 	"item_magic_wand", --大魔棒7.14
-	"item_spirit_vessel", --大骨灰7.07
-	"item_glimmer_cape",
-	"item_blink", --跳刀
-	"item_cyclone", --风杖
+	"item_power_treads",
+	"item_veil_of_discord", --大骨灰7.07
+	"item_shivas_guard",
+	"item_kaya_and_sange",
+	"item_heart",
 	"item_ultimate_scepter", --蓝杖
-	"item_wind_waker",
 	"item_ultimate_scepter_2",
 	"item_sheepstick",
-	"item_overwhelming_blink",
+	"item_octarine_core",
+	"item_travel_boots_2",
 }
 
 return X
