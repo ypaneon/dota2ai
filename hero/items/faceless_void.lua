29886cda859f96af426de3ca4a15ed927f82d026
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 11:23:19 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/faceless_void.lua
+++ hero/items/faceless_void.lua
@@ -7,22 +7,20 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_slippers",
-	"item_circlet",
-	"item_slippers",
-	"item_circlet",
 	"item_wraith_band",
+	"item_magic_wand", --大魔棒7.14
 	"item_wraith_band",
 	"item_power_treads", --假腿7.21
-	"item_magic_wand", --大魔棒7.14
-	-- "item_orb_of_corrosion",
+	"item_javelin",
 	"item_maelstrom",
-	"item_manta", --分身
-	"item_black_king_bar", --bkb
+	"item_sange_and_yasha",
 	"item_mjollnir",
-	"item_monkey_king_bar", --金箍棒7.14
 	"item_greater_crit",
 	"item_butterfly" --蝴蝶
+	"item_satanic",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
