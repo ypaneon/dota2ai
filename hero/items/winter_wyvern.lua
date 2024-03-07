80d3f72d65ffec49083ed64db5f97847da1a9fe4
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 18:39:19 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/winter_wyvern.lua
+++ hero/items/winter_wyvern.lua
@@ -7,18 +7,22 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
-	"item_clarity",
-	"item_magic_stick",
-	"item_tranquil_boots",
-	"item_urn_of_shadows",
-	"item_glimmer_cape", --微光
-	"item_force_staff",
-	"item_aghanims_shard",
+	"item_null_talisman",
+	"item_magic_wand",
+	"item_power_treads",
+	"item_witch_blade",
+	"item_javelin",
+	"item_maelstrom",
 	"item_ultimate_scepter", --蓝杖
-	"item_lotus_orb", --清莲宝珠
+	"item_mjollnir",
+	"item_force_staff",
+	"item_devastator",
+	"item_hurricane_pike",
+	"item_aghanims_shard",
+	"item_moon_shard",
 	"item_ultimate_scepter_2",
-	"item_sheepstick" --羊刀
+	"item_skadi",
+	"item_travel_boots_2",
 }
 
 return X
