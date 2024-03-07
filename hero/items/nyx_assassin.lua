fdff0ab2bdd34e18ff3d44218843314401466812
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 15:35:03 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/nyx_assassin.lua
+++ hero/items/nyx_assassin.lua
@@ -8,22 +8,21 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_enchanted_mango",
-	"item_flask",
-	"item_magic_stick", --大魔棒7.14
-	"item_boots",
+	"item_magic_wand", --大魔棒7.14
 	"item_arcane_boots",
-	"item_magic_wand",
-	"item_hand_of_midas", --点金
-	"item_ultimate_scepter", --蓝杖
+	"item_phylactery",
 	"item_dagon",
-	"item_force_staff", --推推
-	"item_guardian_greaves",
+	"item_kaya",
+	"item_angels_demise",
+	"item_yasha_and_kaya",
 	"item_dagon_3",
-	"item_overwhelming_blink",
-	"item_ultimate_scepter_2",
 	"item_sheepstick",
 	"item_dagon_5",
+	"item_heart",
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
