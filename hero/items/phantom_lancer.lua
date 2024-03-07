567e66193b3ed0ce065c05d8c59533ca466b189f
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 16:11:11 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/phantom_lancer.lua
+++ hero/items/phantom_lancer.lua
@@ -8,17 +8,20 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_tango",
 	"item_wraith_band",
 	"item_magic_wand",
 	"item_wraith_band",
 	"item_power_treads", --假腿7.21
 	"item_diffusal_blade", --散失刀
 	"item_manta",
+	"item_ultimate_scepter",
 	"item_heart", --龙心7.20
-	"item_aghanims_shard",
+	"item_disperser",
 	"item_skadi", --冰眼
-	"item_butterfly"
+	"item_ultimate_scepter_2",
+	"item_butterfly",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
