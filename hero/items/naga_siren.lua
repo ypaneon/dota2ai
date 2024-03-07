326f3c8e67e384f55c3cb8059a81e4ea1dd883d9
ypaneon <148877374+ypaneon@users.noreply.github.com>
2024-03-07 15:16:58 +0630

    Update the builds of some heroes to 7.35c

--- hero/items/naga_siren.lua
+++ hero/items/naga_siren.lua
@@ -8,18 +8,21 @@ local X = {}
 X.ItemsToBuy =
 {
 	"item_tango",
-	"item_flask",
-	"item_magic_stick",
-	"item_tranquil_boots",
-	"item_urn_of_shadows",
-	"item_solar_crest", --大勋章7.20
-	"item_ultimate_scepter",
-	"item_aghanims_shard",
-	"item_guardian_greaves",
-	"item_pipe",
-	"item_lotus_orb", --清莲宝珠
-	"item_ultimate_scepter_2",
+	"item_wraith_band",
+	"item_magic_wand",
+	"item_wraith_band",
+	"item_power_treads",
 	"item_diffusal_blade" --散失刀
+	"item_manta",
+	"item_vladmir",
+	"item_orchid",
+	"item_heart",
+	"item_bloodthorn",
+	"item_disperser",
+	"item_ultimate_scepter",
+	"item_ultimate_scepter_2",
+	"item_aghanims_shard",
+	"item_travel_boots_2",
 }
 
 return X
